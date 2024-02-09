59c15d8b67e2ba2daea9b61d558e0bf37e33843d
ayush pandey <ayushpandey@ayushs-MacBook-Air-2.local>
2024-02-09 13:00:03 +0530

    f

--- slider.js
+++ slider.js
@@ -1,4 +1,27 @@
 const a = document.querySelectorAll('.slider');
-// var counter = 0
+var counter = 0
 
+// console.log(a);
 console.log(a);
+a.forEach(
+    (slider,index) =>
+    {
+        slider.style.left=`${index*100}%`
+    }
+)
+
+const Previous=()=>{
+    counter--
+    sliderrun()
+}
+const Next=()=>{
+    counter++
+    sliderrun()
+}
+const sliderrun=()=>
+{
+    a.forEach(
+        (slider)=>
+        slider.style.transform=`translateX(-${counter*100}%`
+    )
+}
